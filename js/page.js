6f369cb4dbe3d514418fe25b01559a0de2b31539
Alexandru Gatea <alexandru.gatea@freshbyteinc.com>
2019-03-28 13:07:57 +0200

    fixed animations

--- js/page.js
+++ js/page.js
@@ -1,82 +1,75 @@
 jQuery(document).ready(function($){
     var itemToAnimate = $('.page .page-item');
     var itemTrigger = $('.page .page-number a');
+
+    var animationLeftIn = 'fadeInLeft';
+    var animationRightIn = 'fadeInRight';
+    var animationLeftOut = 'fadeOutLeft';
+    var animationRightOut = 'fadeOutRight';
+    
     addItemDelay(itemToAnimate);
     setActivepageNumber();
 
-    animateIn(itemToAnimate);
+    var urlParam = new URLSearchParams(window.location.search);
+    var direction = urlParam.get('direction');
+    if (direction == 'left') {
+        animateIn(itemToAnimate, animationLeftIn);
+    } else {
+        animateIn(itemToAnimate, animationRightIn);
+    }
 
-    navigate();
-
-    itemTrigger.on('click', function(e){
+    itemTrigger.on('click', function(e) {
         e.preventDefault();
+        itemToAnimate.removeClass(animationLeftIn).removeClass(animationLeftIn);
+        var direction = $(this).attr('href').split('=')[1];
         var toGo = $(this).attr('href');
-        animateOut(itemToAnimate);
+        if (direction == 'left') {
+            animateOut(itemToAnimate, animationRightOut);
+        } else {
+            animateOut(itemToAnimate, animationLeftOut);
+        }
 
         setTimeout(function(){
             window.location.replace(toGo);
         }, 1500)
     });
 
+    function addItemDelay(item) {
+        if ($('.page').length > 0) {
+    
+            var delay = 100;
+            item.each(function(){
+                $(this).css({
+                    animationDelay: delay + 'ms'
+                }).addClass('animated');
+                delay += 100;
+            });
+        }
+    }
+    
+    function setActivepageNumber() {
+        var currentNumber = parseInt(getCurrentPageNumber());
+        $('.page li.page-number:nth-child(' + currentNumber + ')').addClass('active');
+    }
+    
+    function getCurrentPageNumber() {
+        var pageNumber;
+        if (window.location.href.indexOf('/page/') > 0) {
+            var url =  window.location.href;
+            var current = url.split('/page/')[1];
+             pageNumber = current.split('.')[0];
+            }
+        return pageNumber;
+    }
+    
+    
+    function animateIn(element, animation) {
+        element.addClass(animation);
+    }
+    
+    function animateOut(element, toAdd) {
+        element.addClass(toAdd);
+    }
 
 });
 
-function addItemDelay(item) {
-    if ($('.page').length > 0) {
-
-        var delay = 100;
-        item.each(function(){
-            $(this).css({
-                animationDelay: delay + 'ms'
-            });
-            delay += 100;
-        });
-    }
-}
-
-function setActivepageNumber() {
-    var currentNumber = getCurrentPageNumber();
-    $('.page li.page-number:nth-child(' + currentNumber + ')').addClass('active');
-}
-
-function getCurrentPageNumber() {
-    var pageNumber;
-    if (window.location.href.indexOf('/page/') > 0) {
-        var url =  window.location.href;
-        var current = url.split('/page/')[1];
-         pageNumber = current.split('.')[0];
-        }
-    return pageNumber;
-}
-
-function navigate() {
-    var navTrigger = $('.page .page-numbers [data-target]');
-
-    navTrigger.on('click', function(){
-
-        var clicked = $(this).attr('data-target');
-        var pn = parseInt(getCurrentPageNumber());
-
-        if (clicked == "previous" && pn > 1) {
-            var url = window.location.href.split(pn)[0];
-            var urlExt = window.location.href.split(pn)[1];
-
-            window.location.replace(url + (pn - 1) + urlExt);
-
-        } else if (clicked == "next" && pn < 3) {
-            var url = window.location.href.split(pn)[0];
-            var urlExt = window.location.href.split(pn)[1];
-
-            window.location.replace(url + (pn + 1) + urlExt);
-
-        }
-    })
-}
-
-function animateIn(element) {
-    element.addClass('animated zoomIn');
-}
-
-function animateOut(element) {
-    element.removeClass('zoomIn').addClass('zoomOut');
-}
